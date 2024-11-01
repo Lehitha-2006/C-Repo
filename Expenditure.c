da1d266993152d0cb4d69c4bbdff9c62f71a697e
Lehitha-2006 <addagarlalehitha@gmail.com>
2024-11-02 00:22:45 +0530

    Uploading code: Expenditure.c

---
+++ Expenditure.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int X,Y;
+    scanf("%d",&X);
+    scanf("%d",&Y);
+    if(X/Y>=30)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    return 0;
+}
