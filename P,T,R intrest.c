8a49cd5aaba10c231b4ce47d211e030bd403b67f
Lehitha-2006 <addagarlalehitha@gmail.com>
2026-01-05 18:48:52 +0530

    Add files via upload

---
+++ P,T,R intrest.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+	int P,T,R,intrest;
+	printf("enter P,T,R values");
+	scanf("%d%d",&P,&T,&R);
+	intrest=(P*T*R)/100;
+	printf("int is %d",intrest);
+}
---
+++ b,h area.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+	float b,h,area;
+	printf("enter b,h values");
+	scanf("%f%f",&b,&h);
+	area=0.5*b*h;
+	printf("area is %f",area);
+}
